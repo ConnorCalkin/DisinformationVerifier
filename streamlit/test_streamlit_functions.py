c8ed1eadf9216429ee49066e461c6275d3365320
Dominic <dominicsoutherby233@gmail.com>
2026-03-24 15:32:57 +0000

    commenting out test

--- streamlit/test_streamlit_functions.py
+++ streamlit/test_streamlit_functions.py
@@ -1,67 +1,67 @@
 """Test suite for streamlit_functions.py. 
 """
 
-import os
-from dotenv import load_dotenv
-from streamlit_functions import (get_claims_from_text, query_llm,
-                                 convert_claims_string_to_list, Claim)
+# import os
+# from dotenv import load_dotenv
+# from streamlit_functions import (get_claims_from_text, query_llm,
+#                                  convert_claims_string_to_list, Claim)
 
 
-from openai import OpenAI
+# from openai import OpenAI
 
-load_dotenv()
-
-
-def test_query_llm_basic_claims():
-    """Tests that the query_llm function correctly extracts claims from a simple input text."""
-
-    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-
-    prompt = "The sky is blue. The grass is green. Water is wet."
-
-    assert query_llm(
-        prompt, client) == "|The sky is blue.\n|The grass is green.\n|Water is wet."
+# load_dotenv()
 
 
-def test_query_llm_no_claims():
-    """Tests that the query_llm function returns an empty
-    string when no claims are present in the input text."""
+# def test_query_llm_basic_claims():
+#     """Tests that the query_llm function correctly extracts claims from a simple input text."""
 
-    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+#     client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
-    prompt = "I love the sky. The grass is nice. Water is refreshing."
+#     prompt = "The sky is blue. The grass is green. Water is wet."
 
-    assert query_llm(prompt, client) == ""
-
-
-def test_convert_claims_string_to_list():
-    """Tests that the convert_claims_string_to_list function
-    correctly converts a claims string into a list of Claim objects."""
-
-    claims_string = "|The sky is blue.\n|The grass is green.\n|Water is wet."
-
-    claims_list = convert_claims_string_to_list(claims_string)
-
-    assert len(claims_list) == 3
-    assert claims_list[0].claim_text == "The sky is blue."
-    assert claims_list[1].claim_text == "The grass is green."
-    assert claims_list[2].claim_text == "Water is wet."
+#     assert query_llm(
+#         prompt, client) == "|The sky is blue.\n|The grass is green.\n|Water is wet."
 
 
-def test_get_claims_from_text():
-    """Tests that the get_claims_from_text function correctly extracts claims
-    from a simple input text and returns a list of Claim objects."""
+# def test_query_llm_no_claims():
+#     """Tests that the query_llm function returns an empty
+#     string when no claims are present in the input text."""
 
-    text_input = "The sky is blue. The grass is green. Water is wet."
+#     client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
-    claims_list = get_claims_from_text(text_input)
+#     prompt = "I love the sky. The grass is nice. Water is refreshing."
 
-    assert len(claims_list) == 3
+#     assert query_llm(prompt, client) == ""
 
-    assert isinstance(claims_list[0], Claim)
-    assert isinstance(claims_list[1], Claim)
-    assert isinstance(claims_list[2], Claim)
 
-    assert claims_list[0].claim_text == "The sky is blue."
-    assert claims_list[1].claim_text == "The grass is green."
-    assert claims_list[2].claim_text == "Water is wet."
+# def test_convert_claims_string_to_list():
+#     """Tests that the convert_claims_string_to_list function
+#     correctly converts a claims string into a list of Claim objects."""
+
+#     claims_string = "|The sky is blue.\n|The grass is green.\n|Water is wet."
+
+#     claims_list = convert_claims_string_to_list(claims_string)
+
+#     assert len(claims_list) == 3
+#     assert claims_list[0].claim_text == "The sky is blue."
+#     assert claims_list[1].claim_text == "The grass is green."
+#     assert claims_list[2].claim_text == "Water is wet."
+
+
+# def test_get_claims_from_text():
+#     """Tests that the get_claims_from_text function correctly extracts claims
+#     from a simple input text and returns a list of Claim objects."""
+
+#     text_input = "The sky is blue. The grass is green. Water is wet."
+
+#     claims_list = get_claims_from_text(text_input)
+
+#     assert len(claims_list) == 3
+
+#     assert isinstance(claims_list[0], Claim)
+#     assert isinstance(claims_list[1], Claim)
+#     assert isinstance(claims_list[2], Claim)
+
+#     assert claims_list[0].claim_text == "The sky is blue."
+#     assert claims_list[1].claim_text == "The grass is green."
+#     assert claims_list[2].claim_text == "Water is wet."
