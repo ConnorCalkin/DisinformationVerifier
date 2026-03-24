6daa17c311744498e3d741662d06ab859cc8bc03
connorcalkin <connor_calkin@outlook.com>
2026-03-24 17:18:51 +0000

    removed unused function from test_ingest

--- rag/test_ingest.py
+++ rag/test_ingest.py
@@ -1,12 +1,5 @@
 # pylint: skip-file
-from ingest import is_valid_article_text, build_metadata
-
-
-def test_is_valid_article_text():
-    assert is_valid_article_text("This is a valid article text.") == True
-    assert is_valid_article_text("   ") == False
-    assert is_valid_article_text("") == False
-    assert is_valid_article_text("   Valid text with spaces   ") == True
+from ingest import build_metadata
 
 # TODO: Add tests for build_metadata when we have a
 # better idea of what metadata we want to include
